6f1f50856533a5dca53b1f9fcec29f525011c387
Samuel C. Payson <scpayson@gmail.com>
2011-03-06 02:42:40 -0500

    Fixed GPL type and added to new files.
    There was a typo in the GPL header of all files, saying "at you
    option" instead of "at your option". Corrected this and added the
    GPL to all *.h files, none of which had it.

--- include/gfx.h
+++ include/gfx.h
@@ -1,4 +1,24 @@
 /* gfx.h */
+
+/*****************************************************************************
+ * This file is part of the automaton project.                               *
+ * Copyright (C) 2011 Samuel C. Payson, Timothy Kopp,                        *
+ *                    Akanksha Vyas,    Mark Platek                          *
+ *                                                                           *
+ * Automaton is free software: you can redistribute it and/or modify it      *
+ * under  the terms of the GNU General Public License as published by the    *
+ * Free Software Foundation, either version 3 of the License, or (at your    *
+ * option) any later version.                                                *
+ *                                                                           *
+ * Automata is distributed in the hope that it will be useful, but WITHOUT   *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     *
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for *
+ * more details.                                                             *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License along   *
+ * with automaton. If not, see <http://www.gnu.org/licenses/>.               *
+ *****************************************************************************/
+
 #ifndef _AUTOMATON_GFX_H_
 #define _AUTOMATON_GFX_H_
 
